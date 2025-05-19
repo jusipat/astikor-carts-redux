a8f87e20057e3c430670cbe078febbcdba6406ce
jusipat <justin10patrick@gmail.com>
2025-05-18 20:47:31 -0400

    CartItem refactor

--- src/main/java/de/mennomax/astikorcarts/item/CartItem.java
+++ src/main/java/de/mennomax/astikorcarts/item/CartItem.java
@@ -2,14 +2,16 @@ package de.mennomax.astikorcarts.item;
 
 import de.mennomax.astikorcarts.AstikorCarts;
 import net.minecraft.ChatFormatting;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntitySelector;
+import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -45,11 +47,11 @@ public final class CartItem extends Item {
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(final Level world, final Player player, final InteractionHand hand) {
+    public InteractionResult use(Level world, Player player, InteractionHand hand) {
         final ItemStack stack = player.getItemInHand(hand);
         final BlockHitResult result = getPlayerPOVHitResult(world, player, ClipContext.Fluid.ANY);
         if (result.getType() == HitResult.Type.MISS) {
-            return InteractionResultHolder.pass(stack);
+            return InteractionResult.PASS;
         } else {
             final Vec3 lookVec = player.getLookAngle();
             final List<Entity> list = world.getEntities(player, player.getBoundingBox().expandTowards(lookVec.scale(5.0D)).inflate(5.0D), EntitySelector.NO_SPECTATORS.and(Entity::canBeCollidedWith));
@@ -58,24 +60,24 @@ public final class CartItem extends Item {
                 for (final Entity entity : list) {
                     final AABB axisalignedbb = entity.getBoundingBox().inflate(entity.getPickRadius());
                     if (axisalignedbb.contains(eyePos)) {
-                        return InteractionResultHolder.pass(stack);
+                        return InteractionResult.PASS;
                     }
                 }
             }
 
             if (result.getType() == HitResult.Type.BLOCK) {
-                final EntityType<?> type = BuiltInRegistries.ENTITY_TYPE.get(BuiltInRegistries.ITEM.getKey(this));
+                final Holder.Reference<EntityType<?>> type = BuiltInRegistries.ENTITY_TYPE.get(BuiltInRegistries.ITEM.getKey(this)).get();
                 if (type == null) {
-                    return InteractionResultHolder.pass(stack);
+                    return InteractionResult.PASS;
                 }
-                final Entity cart = type.create(world);
+                final Entity cart = type.value().create(world, EntitySpawnReason.SPAWN_ITEM_USE);
                 if (cart == null) {
-                    return InteractionResultHolder.pass(stack);
+                    return InteractionResult.PASS;
                 }
                 cart.moveTo(result.getLocation().x, result.getLocation().y, result.getLocation().z);
                 cart.setYRot((player.getYRot() + 180) % 360);
                 if (!world.noCollision(cart, cart.getBoundingBox().inflate(0.1F, -0.1F, 0.1F))) {
-                    return InteractionResultHolder.fail(stack);
+                    return InteractionResult.PASS;
                 } else {
                     if (!world.isClientSide()) {
                         world.addFreshEntity(cart);
@@ -85,11 +87,60 @@ public final class CartItem extends Item {
                         stack.shrink(1);
                     }
                     player.awardStat(Stats.ITEM_USED.get(this));
-                    return InteractionResultHolder.success(stack);
+                    return InteractionResult.SUCCESS;
                 }
             } else {
-                return InteractionResultHolder.pass(stack);
+                return InteractionResult.PASS;
             }
         }
     }
+
+    //    @Override
+//    public InteractionResultHolder<ItemStack> use(final Level world, final Player player, final InteractionHand hand) {
+//        final ItemStack stack = player.getItemInHand(hand);
+//        final BlockHitResult result = getPlayerPOVHitResult(world, player, ClipContext.Fluid.ANY);
+//        if (result.getType() == HitResult.Type.MISS) {
+//            return InteractionResultHolder.pass(stack);
+//        } else {
+//            final Vec3 lookVec = player.getLookAngle();
+//            final List<Entity> list = world.getEntities(player, player.getBoundingBox().expandTowards(lookVec.scale(5.0D)).inflate(5.0D), EntitySelector.NO_SPECTATORS.and(Entity::canBeCollidedWith));
+//            if (!list.isEmpty()) {
+//                final Vec3 eyePos = player.getEyePosition(1.0F);
+//                for (final Entity entity : list) {
+//                    final AABB axisalignedbb = entity.getBoundingBox().inflate(entity.getPickRadius());
+//                    if (axisalignedbb.contains(eyePos)) {
+//                        return InteractionResultHolder.pass(stack);
+//                    }
+//                }
+//            }
+//
+//            if (result.getType() == HitResult.Type.BLOCK) {
+//                final EntityType<?> type = BuiltInRegistries.ENTITY_TYPE.get(BuiltInRegistries.ITEM.getKey(this));
+//                if (type == null) {
+//                    return InteractionResultHolder.pass(stack);
+//                }
+//                final Entity cart = type.create(world);
+//                if (cart == null) {
+//                    return InteractionResultHolder.pass(stack);
+//                }
+//                cart.moveTo(result.getLocation().x, result.getLocation().y, result.getLocation().z);
+//                cart.setYRot((player.getYRot() + 180) % 360);
+//                if (!world.noCollision(cart, cart.getBoundingBox().inflate(0.1F, -0.1F, 0.1F))) {
+//                    return InteractionResultHolder.fail(stack);
+//                } else {
+//                    if (!world.isClientSide()) {
+//                        world.addFreshEntity(cart);
+//                        world.playSound(null, cart.getX(), cart.getY(), cart.getZ(), AstikorCarts.SoundEvents.CART_PLACED.get(), SoundSource.BLOCKS, 0.75F, 0.8F);
+//                    }
+//                    if (!player.getAbilities().instabuild) {
+//                        stack.shrink(1);
+//                    }
+//                    player.awardStat(Stats.ITEM_USED.get(this));
+//                    return InteractionResultHolder.success(stack);
+//                }
+//            } else {
+//                return InteractionResultHolder.pass(stack);
+//            }
+//        }
+//    }
 }
